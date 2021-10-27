606672b42481a8d96d0c81c998ee5b7da0dbb9c6
Jon Courtier <jonaqwerty@yahoo.com>
2021-10-27 14:55:30 -0400

    More CSS

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <div className="header">
             <h1>Welcome to Canine Crushes</h1>
+            <img src="https://www.newportharborvets.com/sites/default/files/styles/large/public/golden-retriever-dog-breed-info.jpg?itok=LraUUWYV" alt="header-img"/>
             <div className="header-text">
             <p>Your one-stop destination to eyeballing some seriously precious pups!</p>
             </div>
